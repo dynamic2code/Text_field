ae7348a604e8594a05e860f7b3b945e7c51a4dd8
dynamic2code <dynamicdesingns@gmail.com>
2023-05-03 00:47:31 +0300

    test completed writes you were there

--- app/src/main/java/com/example/textfield/MainActivity.kt
+++ app/src/main/java/com/example/textfield/MainActivity.kt
@@ -4,18 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.textfield.ui.theme.TextfieldTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,14 +44,30 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Text_field(){
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
             .padding(30.dp)
     ) {
-        val
-        TextField(value = "I am here", onValueChange = {} )
+        var name by remember {
+            mutableStateOf("")
+        }
+        TextField(value = name, onValueChange = {newname->
+            name = newname
+        },
+            label = {
+                Text(text = "name")
+            },
+            singleLine = true
+        )
+        Spacer(modifier = Modifier.size(30.dp))
+        Text(text = "list of I was Here's")
+        Text(text = "$name was here")
     }
 
 }
+
+
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
